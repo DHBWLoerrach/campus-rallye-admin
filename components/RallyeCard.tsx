8cd53b8aebd1de2bf9cf420c3a7b01e20d74ef04
Erik Behrends <behrends@users.noreply.github.com>
2024-07-15 16:27:49 +0200

    Add date time picker for rallye end time to cards

--- components/RallyeCard.tsx
+++ components/RallyeCard.tsx
@@ -1,11 +1,14 @@
 'use client';
 
+import { useState } from 'react';
+import { de } from 'date-fns/locale';
 import {
   Card,
   CardContent,
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
+import { DateTimePicker } from '@/components/ui/datetime-picker';
 import { Label } from '@/components/ui/label';
 import { Switch } from '@/components/ui/switch';
 import {
@@ -14,6 +17,9 @@ import {
 } from '@/components/ui/toggle-group';
 
 export default function RallyeCard({ rallye }) {
+  const [date24, setDate24] = useState<Date>(
+    new Date(rallye.end_time)
+  );
   return (
     <Card key={rallye.id}>
       <CardHeader>
@@ -60,7 +66,15 @@ export default function RallyeCard({ rallye }) {
           </ToggleGroup>
         </div>
         <div className="flex items-center space-x-2 mt-2">
-          Ende der Rallye: {rallye.end_time}
+          <Label htmlFor={`rallye-${rallye.id}-endtime`}>
+            Ende der Rallye
+          </Label>
+          <DateTimePicker
+            locale={de}
+            hourCycle={24}
+            value={date24}
+            onChange={setDate24}
+          />
         </div>
       </CardContent>
     </Card>
