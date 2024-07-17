8d3092e620a4b6182c7f50d21f8339181c1d6239
Erik Behrends <behrends@users.noreply.github.com>
2024-07-17 16:16:45 +0200

    Add navigation header

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,15 @@
 import { createClient } from '@/lib/supabase/server';
-import { signOut } from '@/actions/auth';
 import Rallye from '@/components/Rallye';
 
 export default async function Home() {
   const supabase = createClient();
   const { data: rallyes } = await supabase.from('rallye').select();
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
 
   return (
-    <>
-      <p>Hallo, {user.email}</p>
-      <form action={signOut}>
-        <button
-          className="bg-blue-500 text-white p-2 rounded hover:bg-blue-600"
-          type="submit"
-        >
-          Abmelden
-        </button>
-      </form>
-      <main className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 m-4">
-        {rallyes.map((rallye) => (
-          <Rallye key={rallye.id} rallye={rallye} />
-        ))}
-      </main>
-    </>
+    <main className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 m-4">
+      {rallyes.map((rallye) => (
+        <Rallye key={rallye.id} rallye={rallye} />
+      ))}
+    </main>
   );
 }
---
+++ components/Navigation.tsx
@@ -0,0 +1,68 @@
+import { createClient } from '@/lib/supabase/server';
+import { signOut } from '@/actions/auth';
+import { LogOut, Menu } from 'lucide-react';
+import { Route } from '@/lib/types';
+import { Button } from '@/components/ui/button';
+import {
+  Sheet,
+  SheetContent,
+  SheetTrigger,
+} from '@/components/ui/sheet';
+import NavItems from '@/components/NavItems';
+
+const routes: Route[] = [
+  {
+    href: '/',
+    label: 'Rallyes',
+  },
+  {
+    href: '/admin',
+    label: 'Administration',
+  },
+];
+
+export default async function Nav() {
+  const supabase = createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) return null; // no navigation for unauthenticated users
+
+  return (
+    <header className="sticky top-0 flex justify-between items-center h-16 border-b bg-background px-6">
+      <nav className="hidden flex-col sm:flex sm:flex-row sm:items-center gap-2 text-sm font-medium">
+        <NavItems routes={routes} />
+      </nav>
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button
+            variant="outline"
+            size="icon"
+            className="shrink-0 sm:hidden"
+          >
+            <Menu className="h-5 w-5" />
+            <span className="sr-only">
+              Navigationsmenü ein-/ausblenden
+            </span>
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="left">
+          <nav className="grid gap-6 font-medium">
+            <NavItems routes={routes} />
+          </nav>
+        </SheetContent>
+      </Sheet>
+      <div className="flex flex-1 flex-row  justify-end gap-2">
+        {user && (
+          <form action={signOut}>
+            <Button variant="outline">
+              <span className="hidden sm:block">Abmelden</span>
+              <LogOut className="sm:ml-2 h-[1.2rem] w-[1.2rem]" />
+            </Button>
+          </form>
+        )}
+      </div>
+    </header>
+  );
+}
