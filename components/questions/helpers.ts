8aeac1a1ef019d9912ed667c273900b3728c4563
limo710 <79151183+limo710@users.noreply.github.com>
2025-02-03 15:02:32 +0100

    Deactivating upload - no uplaod in ne db

--- components/questions/helpers.ts
+++ components/questions/helpers.ts
@@ -3,5 +3,5 @@ export const questionTypes = [
   { id: "multiple_choice", name: "Multiple Choice" },
   { id: "picture", name: "Bild" },
   { id: "qr_code", name: "QR Code" },
-  { id: "upload", name: "Upload" },
+  // { id: "upload", name: "Upload" },
 ];
