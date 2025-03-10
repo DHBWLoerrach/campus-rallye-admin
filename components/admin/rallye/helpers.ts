8101922d027ed101e2da6c2da315ad0c28546151
Erik Behrends <behrends@users.noreply.github.com>
2025-03-10 15:00:56 +0100

    Remove react-admin

--- components/admin/rallye/helpers.ts
+++
@@ -1,6 +0,0 @@
-export const rallyeStatus = [
-  { id: "preparation", name: "Vorbereitung" },
-  { id: "running", name: "Gestartet" },
-  { id: "post_processing", name: "Nachbereitung" },
-  { id: "ended", name: "Beendet" },
-];
