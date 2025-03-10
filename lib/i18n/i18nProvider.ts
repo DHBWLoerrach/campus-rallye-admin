9b4222a7e4cb4a169f2c4b251073a223a3939b6e
Erik Behrends <behrends@users.noreply.github.com>
2025-03-10 17:19:10 +0100

    Remove obsolete libs

--- lib/i18n/i18nProvider.ts
+++
@@ -1,8 +0,0 @@
-import { mergeTranslations } from "ra-core";
-import polyglotI18nProvider from "ra-i18n-polyglot";
-import de from "./ra-language-german";
-import { raSupabaseGermanMessages } from "./ra-supabase-language-german";
-
-const allGermanMessages = mergeTranslations(de, raSupabaseGermanMessages);
-
-export const i18nProvider = polyglotI18nProvider(() => allGermanMessages, "de");
