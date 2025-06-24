31262fcd7a7ed4e6a556aa5c477ebc63b6aa2baa
Erik Behrends <behrends@users.noreply.github.com>
2025-06-24 18:35:13 +0200

    Remove obsolete functions

--- actions/multipleChoiceAnswers.ts
+++ actions/multipleChoiceAnswers.ts
@@ -5,61 +5,6 @@ import createClient from '@/lib/supabase';
 
 type FormState = { errors?: { message?: string } } | undefined;
 
-export async function createMultipleChoiceAnswers(
-  state: FormState,
-  formData: FormData
-) {
-  await requireProfile();
-  const supabase = await createClient();
-
-  const data = { name: formData.get('name') as string };
-
-  const { error } = await supabase.from('rallye').insert({
-    name: data.name,
-    is_active_rallye: false,
-    status: 'preparation',
-  });
-
-  if (error) {
-    return { errors: { message: 'Es ist ein Fehler aufgetreten' } };
-  }
-
-  revalidatePath('/');
-  return { success: { message: 'Rallye erfolgreich gespeichert' } };
-}
-
-export async function updateMultipleChoiceAnswers(
-  state: FormState,
-  formData: FormData
-) {
-  const supabase = await createClient();
-
-  const data = {
-    id: formData.get('id') as string,
-    name: formData.get('name') as string,
-    is_active_rallye: formData.get('active') === 'on', // checkbox value needs to be converted to boolean (might be 'on' or null)
-    status: formData.get('status') as string,
-    end_time: new Date(formData.get('end_time') as string),
-  };
-
-  const { error } = await supabase
-    .from('rallye')
-    .update({
-      name: data.name,
-      is_active_rallye: data.is_active_rallye,
-      status: data.status,
-      end_time: data.end_time,
-    })
-    .eq('id', data.id);
-
-  if (error) {
-    return { errors: { message: 'Es ist ein Fehler aufgetreten' } };
-  }
-
-  revalidatePath('/');
-  return { success: { message: 'Rallye erfolgreich gespeichert' } };
-}
-
 export async function getChildren(id) {
   // console.log(id);
   const supabase = await createClient();
